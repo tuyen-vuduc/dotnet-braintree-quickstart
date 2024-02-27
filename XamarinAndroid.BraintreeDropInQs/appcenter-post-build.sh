d6c6ba5813c86b516a3d9fb6b97c5062bcc6bb3b
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-02-28 06:56:25 +0700

    - try installing .NET 7 via script

--- XamarinAndroid.BraintreeDropInQs/appcenter-post-build.sh
+++ XamarinAndroid.BraintreeDropInQs/appcenter-post-build.sh
@@ -1,1 +1,11 @@
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --channel 7.0
+
+dotnet --list-sdks
+
+echo "Install .NET Android workdload"
+dotnet workload install android
+
+echo "Build project"
 dotnet build ../DotNetAndroid.BraintreeDropInQs/DotNetAndroid.BraintreeDropInQs.csproj -c Release -v normal
