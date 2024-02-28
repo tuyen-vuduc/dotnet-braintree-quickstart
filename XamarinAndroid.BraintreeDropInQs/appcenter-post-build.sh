9ce61c1a2c794a70bdb4ee366287ae8e9ef9ec70
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-02-28 07:06:11 +0700

    - remove installing .NET script in postbuild

--- XamarinAndroid.BraintreeDropInQs/appcenter-post-build.sh
+++ XamarinAndroid.BraintreeDropInQs/appcenter-post-build.sh
@@ -1,11 +1,5 @@
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh --channel 7.0
-
+echo "List .NET SDKs"
 dotnet --list-sdks
-
-echo "Install .NET Android workdload"
-dotnet workload install android
 
 echo "Build project"
 dotnet build ../DotNetAndroid.BraintreeDropInQs/DotNetAndroid.BraintreeDropInQs.csproj -c Release -v normal
--- XamarinAndroid.BraintreeDropInQs/appcenter-post-clone.sh
+++ XamarinAndroid.BraintreeDropInQs/appcenter-post-clone.sh
@@ -1,3 +1,6 @@
 wget https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --channel 7.0 --install-dir "$AGENT_TOOLSDIRECTORY/dotnet"
+
+echo "Install .NET Android workdload"
+dotnet workload install android
